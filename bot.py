84fc1e7f4db67498c664b5677c0d001ca21e06fe
Javlonbek Butabaev <butabaev.javlon@gmail.com>
2019-03-07 17:25:17 +0500

    First Commit

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import redis
 import os
 import telebot
+import requests
 # import some_api_lib
 # import ...
 
@@ -11,10 +12,33 @@ token = os.environ['TELEGRAM_TOKEN']
 some_api_token = os.environ['SOME_API_TOKEN']
 #             ...
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
 
 #       Your bot code below
 # bot = telebot.TeleBot(token)
 # some_api = some_api_lib.connect(some_api_token)
 #              ...
+
+bot = telebot.TeleBot(f"{token}")
+artist = ""
+title = ""
+
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+    bot.reply_to(message, "I can help you find lyrics for your songs.\n\nYou can request lyrics by sending me Artis\nand Title name separated by '-'\n\n\nExample:\n\n'Michael Jackson - Thriller'")
+
+@bot.message_handler(content_types=['text'])
+def find_lyrics(message):
+    chat_id = message.chat.id
+    user_input = message.text
+    data_list = user_input.split('-')
+    contents = requests.get(f'https://orion.apiseeds.com/api/music/lyric/{data_list[0]}/{data_list[1]}?apikey={some_api_token}').json()
+    try:
+        lyrics = contents['result']['track']['text']
+    except KeyError:
+        bot.reply_to(message, text="Sorry, I couldn't find lyrics for this song")
+    else:
+        bot.reply_to(message, text=lyrics)
+
+
+
+bot.polling()
